eaa9ec2630e35b2786969ed9bdf2a3d8e8da462d
Mantis-hub <tanawat.panyac@bumail.net>
2025-05-04 09:50:50 +0700

    add audioManager, addscript Audio manager,add all Sound

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+   [Header("Audio Source")]
+   [SerializeField] AudioSource musicsource;
+   [SerializeField] AudioSource sfxsource;
+   
+   [Header("Audio Clips")]
+   public AudioClip backgroundMusic;
+   public AudioClip jump;
+   public AudioClip chargejump;
+   public AudioClip hit;
+   public AudioClip pickup;
+   public AudioClip unlock;
+   
+}
